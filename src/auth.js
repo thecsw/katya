c7a65cb2115a7b7cc7aae1c529bf2007a3bea2ae
Sandy Urazayev <ctu@ku.edu>
2021-10-18 12:53:22 -0500

    Fixes for vercel
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- src/auth.js
+++ src/auth.js
@@ -1,44 +1,46 @@
-import { katya_get, katya_post } from './http.js'
+import { katya_get, katya_post } from "./http.js";
 
-const url = 'https://katya.sandyuraz.com:32000/api'
+const url = "https://katya.sandyuraz.com:32000/api";
 // const url = 'http://127.0.0.1:32000/api'
 
-function getCookie (what) {
-  const value = `; ${document.cookie}`
-  const parts = value.split(`; ${what}=`)
-  if (parts.length === 2) { return parts.pop().split(';').shift() }
+function getCookie(what) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${what}=`);
+        if (parts.length === 2) {
+                return parts.pop().split(";").shift();
+        }
 }
 
-function setCookie (what) {
-  document.cookie = `user=${what};max-age=172800;path=/;`
+function setCookie(what) {
+        document.cookie = `user=${what};max-age=172800;path=/;`;
 }
 
-function encode (user, pass) {
-  return btoa(user + ':' + pass)
+function encode(user, pass) {
+        return btoa(user + ":" + pass);
 }
 
-function check () {
-  const existingCookie = getCookie('user')
-  return login(existingCookie)
+function check() {
+        const existingCookie = getCookie("user");
+        return login(existingCookie);
 }
 
-function login (auth_token) {
-  if (auth_token !== '' && typeof auth_token !== 'undefined') {
-    katya_post(url + '/auth', {}, auth_token).then((data) => {
-      if (data[0] === 200) {
-        setCookie(auth_token)
-        return true
-      }
-    })
-  }
-  return false
+function login(auth_token) {
+        if (auth_token !== "" && typeof auth_token !== "undefined") {
+                katya_post(url + "/auth", {}, auth_token).then((data) => {
+                        if (data[0] === 200) {
+                                setCookie(auth_token);
+                                return true;
+                        }
+                });
+        }
+        return false;
 }
 
-function logout () {
-  setCookie('')
+function logout() {
+        setCookie("");
 }
 
-export const encode_user = encode
-export const check_cookie = check
-export const login_user = login
-export const logout_user = logout
+export const encode_user = encode;
+export const check_cookie = check;
+export const login_user = login;
+export const logout_user = logout;
